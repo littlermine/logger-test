1910baa85693522af8ed8b17895503cab0cd06c5
littlermine <litao8913@gmail.com>
2016-12-29 17:12:29 +0800

    c

--- src/main/java/com/littlemine/test/LoggerController.java
+++ src/main/java/com/littlemine/test/LoggerController.java
@@ -1,26 +1,21 @@
 package com.littlemine.test;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 @RequestMapping(value = "/", method = { RequestMethod.GET, RequestMethod.POST }, produces = { "application/json" })
 public class LoggerController {
 
-    // localhost/Action=LoggerTest
     @ResponseBody
     @RequestMapping(params = { "Action=LoggerTest" })
     public String loggerTest() {
-        TestUtil.log("this is test util");
-        //        log.trace("this is logger test interface..trace");
-        //        log.debug("this is logger test interface..debug");
-        log.info("this is logger test interface..this is logger test interface..this is logger test interface..this is logger test interface..info");
-        //        log.warn("this is logger test interface..warn");
-        //        log.error("this is logger test interface..error");
+        log.info("this is logger test interface..this is logger test interface..this is logger test interface..this is info");
         return "{\"Status\":200}";
     }
 
